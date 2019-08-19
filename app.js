0c028b2edd4d9982392c6339d5ad71b6e1227588
ferdeveloper <fernandoarenasdev@gmail.com>
2019-08-19 22:52:52 +0200

    Change state.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ new Vue({
   el: '#app',
   data () {
     return {
+      isExpanded: false,
       info: null
     }
   },
@@ -9,5 +10,10 @@ new Vue({
     axios
       .get('https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json')
       .then(response => (this.info = response.data))
+  },
+  methods: {
+    toggleClass: function(event){
+      this.isExpanded = !this.isExpanded
+    }
   }
-})
+});
